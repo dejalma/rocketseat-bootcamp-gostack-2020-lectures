ec132cf398f820e598a6c6ccaca76055a579d9fc
dejalma <dejalma.arantes@gmail.com>
2020-04-22 02:45:44 -0300

    code example removed

--- first-node-project/src/routes/index.ts
+++ first-node-project/src/routes/index.ts
@@ -7,15 +7,4 @@ routes.use('/appointments', appointmentsRouter)
 
 routes.get('/', (req, res) => res.json({ message: '🚀 Server started!' }))
 
-routes.post('/users', (req, res) => {
-  const { name, email } = req.body
-
-  const user = {
-    name,
-    email,
-  }
-
-  return res.json(user)
-})
-
 export default routes
